f47bcf6ab00eeef7de49776a8490e1181456b26f
hamedrefaat1 <hamedrefaat10@gmail.com>
2025-08-13 17:55:23 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,9 @@ import 'package:carpooling_app/router/router.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 late String initialRoute;
 void main() async {
@@ -15,10 +17,25 @@ void main() async {
   if (user == null) {
     initialRoute = signUpScreen;
   } else {
-    initialRoute = homeScreen;
+    initialRoute = homeapp;
   }
+
+  //await setUp();
+  MapboxOptions.setAccessToken(
+    "REMOVED",
+  );
   runApp(const CarpoolingApp());
 }
+
+//  Future<void> setUp()async{
+//   await dotenv.load(
+//    fileName: ".env",
+    
+//   );
+//      MapboxOptions.setAccessToken(
+//       dotenv.env["MAPBOX_ACCESS_TOKEN"]!
+//      );
+//  }
 
 class CarpoolingApp extends StatelessWidget {
   const CarpoolingApp({super.key});
@@ -27,7 +44,7 @@ class CarpoolingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-       designSize: const Size(360, 690),
+      // designSize: const Size(360, 690),
       builder: (_, child){
         return MaterialApp(
         theme: AppTheme.lightTheme,
@@ -35,7 +52,7 @@ class CarpoolingApp extends StatelessWidget {
         themeMode: ThemeMode.light,
         debugShowCheckedModeBanner: false,
         onGenerateRoute: AppRouter().generateRoute,
-        initialRoute: signUpScreen,
+        initialRoute:initialRoute,
         home:child ,
       );
       },
--- lib/presentation/screens/homeApp.dart
+++ lib/presentation/screens/homeApp.dart
@@ -1,12 +1,161 @@
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+// import 'package:geolocator/geolocator.dart' as geo;
+
+// class Homeapp extends StatefulWidget {
+//   const Homeapp({super.key});
+
+//   @override
+//   State<Homeapp> createState() => _HomeappState();
+// }
+
+// class _HomeappState extends State<Homeapp> {
+//   MapboxMap? _mapboxMap;
+//   geo.Position? _currentPosition;
+//   StreamSubscription<geo.Position>? _posSub;
+
+//   @override
+//   void initState() {
+    
+//     super.initState();
+//     _initLocationFlow();
+//   }
+
+//   Future<void> _initLocationFlow() async {
+//     // 1) تأكد إن الخدمة شغالة واطلب صلاحيات
+//     if (!await geo.Geolocator.isLocationServiceEnabled()) {
+//       // تقدر تفتح إعدادات الموقع للمستخدم لو حبيت
+//       return;
+//     }
+//     var permission = await geo.Geolocator.checkPermission();
+//     if (permission == geo.LocationPermission.denied) {
+//       permission = await geo.Geolocator.requestPermission();
+//     }
+//     if (permission == geo.LocationPermission.denied ||
+//         permission == geo.LocationPermission.deniedForever) {
+//       return;
+//     }
+
+//     // 2) هات اللوكيشن الأولي
+//     final first = await geo.Geolocator.getCurrentPosition(
+//       desiredAccuracy: geo.LocationAccuracy.high,
+//     );
+//     setState(() => _currentPosition = first);
+
+//     // 3) ستريم تحديثات لايف
+//     _posSub = geo.Geolocator.getPositionStream(
+//       locationSettings: const geo.LocationSettings(
+//         accuracy: geo.LocationAccuracy.high,
+//         distanceFilter: 5,
+//       ),
+//     ).listen((p) {
+//       setState(() => _currentPosition = p);
+//       if (_mapboxMap != null) {
+//         _mapboxMap!.flyTo(
+//           CameraOptions(
+//             center: Point(coordinates: Position(p.longitude, p.latitude)),
+//             zoom: 15,
+//           ),
+//           MapAnimationOptions(duration: 500),
+//         );
+//       }
+//     });
+//   }
+
+//   Future<void> _onMapCreated(MapboxMap controller) async {
+//     _mapboxMap = controller;
+
+//     // فعّل الـ User Location Puck (نقطة موقع المستخدم مع نبض)
+//     await _mapboxMap!.location.updateSettings(
+//       LocationComponentSettings(
+//         enabled: true,
+//         pulsingEnabled: true,
+//         showAccuracyRing: true,
+//       ),
+//     );
+
+//     // وجّه الكاميرا على اللوكيشن الحالي لو موجود
+//     if (_currentPosition != null) {
+//       _mapboxMap!.setCamera(
+//         CameraOptions(
+//           center: Point(
+//             coordinates: Position(
+//               _currentPosition!.longitude,
+//               _currentPosition!.latitude,
+//             ),
+//           ),
+//           zoom: 15,
+//         ),
+//       );
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final hasPos = _currentPosition != null;
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Live Map (Mapbox)'),
+//       ),
+//       body: hasPos
+//           ? MapWidget(
+            
+//               styleUri: MapboxStyles.MAPBOX_STREETS,
+//               cameraOptions: CameraOptions(
+//                 center: Point(
+//                   coordinates: Position(
+//                     _currentPosition!.longitude,
+//                     _currentPosition!.latitude,
+//                   ),
+//                 ),
+//                 zoom: 15,
+//               ),
+//               onMapCreated: _onMapCreated,
+//             )
+//           : const Center(child: CircularProgressIndicator()),
+//       floatingActionButton: hasPos
+//           ? FloatingActionButton(
+//               onPressed: () {
+//                 if (_mapboxMap == null || _currentPosition == null) return;
+//                 _mapboxMap!.easeTo(
+//                   CameraOptions(
+//                     center: Point(
+//                       coordinates: Position(
+//                         _currentPosition!.longitude,
+//                         _currentPosition!.latitude,
+//                       ),
+//                     ),
+//                     zoom: 15,
+//                   ),
+//                   MapAnimationOptions(duration: 400),
+//                 );
+//               },
+//               child: const Icon(Icons.my_location),
+//             )
+//           : null,
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     _posSub?.cancel();
+//     super.dispose();
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class Homeapp extends StatelessWidget {
   const Homeapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-      body: Center(child: Text("wjdnlnSDLNASDLD"),),
+    return  Scaffold(
+      body: MapWidget(),
     );
   }
 }
