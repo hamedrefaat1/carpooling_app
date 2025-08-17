5be1d09394981155c280fe62211138561c7321b8
hamedrefaat1 <hamedrefaat10@gmail.com>
2025-08-17 17:50:33 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:carpooling_app/constants/constStrings.dart';
 import 'package:carpooling_app/constants/themeAndColors.dart';
 import 'package:carpooling_app/firebase_options.dart';
 import 'package:carpooling_app/router/router.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -9,14 +10,30 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart' hide Size;
 
 late String initialRoute;
+late String userType;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   User? user = await FirebaseAuth.instance.authStateChanges().first;
+
   if (user == null) {
     initialRoute = signUpScreen;
+  }
+  DocumentSnapshot userDoc = await FirebaseFirestore.instance
+      .collection("Users")
+      .doc(user!.uid)
+      .get();
+  if (!userDoc.exists) {
+    initialRoute = getUserInfo; 
+  }
+  Map<String, dynamic> userDate = userDoc.data() as Map<String, dynamic>;
+  userType = userDate["type"];
+
+  if (userType == "driver") {
+    initialRoute = driverMainShell;
   } else {
-    initialRoute = homeAppDriver;
+    initialRoute = homeAppRider;
   }
 
   //await setUp();
@@ -26,8 +43,6 @@ void main() async {
   runApp(const CarpoolingApp());
 }
 
-
-
 class CarpoolingApp extends StatelessWidget {
   const CarpoolingApp({super.key});
 
@@ -36,18 +51,17 @@ class CarpoolingApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(360, 690),
-      builder: (_, child){
+      builder: (_, child) {
         return MaterialApp(
-        theme: AppTheme.lightTheme,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.light,
-        debugShowCheckedModeBanner: false,
-        onGenerateRoute: AppRouter().generateRoute,
-        initialRoute:signUpScreen,
-        home:child ,
-      );
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: ThemeMode.light,
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: AppRouter().generateRoute,
+          initialRoute: signUpScreen,
+          home: child,
+        );
       },
-      
     );
   }
 }
